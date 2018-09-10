b2cb7fa3773913322a9a515099e31030f4786144
Larisa <larisa.melqonyan94@gmail.com>
2018-09-10 21:34:22 +0300

    add react map

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+ 
+class GooglMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+ 
+  render() {
+    return (
+      <div style={{ height: '30vh', width: '50%',  margin:"0 auto" }}>
+            <GoogleMapReact
+                className="container karta"
+                bootstrapURLKeys={{
+                    key:'AIzaSyCK6F8nozLf6lsWjxoQK-ATaXsgzI8gjrw',
+                    language: 'en',
+                }}
+                defaultCenter={{lat: 59.95, lng: 30.33}}
+                defaultZoom={11}
+            >
+            </GoogleMapReact>
+      </div>
+    );
+  }
+}
+ 
+export default GooglMap;
